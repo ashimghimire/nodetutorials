d6cab79d433a4c6a25cdbce79a4d54e9f4604a85
ashimghimire <ashimghimire@gmail.com>
2021-08-05 14:25:16 +0545

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,18 @@
+
+var Genre="Jaaz"
+var artist="Tracy Chapman"
+var title="what a beautiful name"
+var durationInMinutes=5
+var isTrending=true
+var hasVideo=true
+var rating=3
+var coverPics="https://unsplash.com/photos/J_mibm6IT3Y"
+
+console.log(Genre);
+console.log(artist);
+console.log(title);
+console.log(durationInMinutes);
+console.log(isTrending);
+console.log(hasVideo);
+console.log(rating);
+console.log(coverPics);
